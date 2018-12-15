33ac5430bb8c9393f6f8064bb61740b7709ae0fd
David Gilbert <dagilbert333@gmail.com>
2018-12-15 21:33:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,14 @@ def reverse_each_word(string)
   arr.each do |word|
     word.reverse!
   end
+<<<<<<< HEAD
   arr.join(" ")
 end
 
 def reverse_each_word(string)
   arr = string.split(" ")
+=======
+>>>>>>> 485a42ad4c27c048a7490ea654fdab15a6bbda42
   arr.collect do |word|
     word.reverse!
   end
